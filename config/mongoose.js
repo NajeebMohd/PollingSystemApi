aac592aada91768d574d9ceca4ea085d70759efc
najeeb <mohdnajeeb9642@gmail.com>
2023-01-12 18:41:34 +0530

    cyclic host

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/PollingSystemAPI');
+//mongoose.connect('mongodb://127.0.0.1/PollingSystemAPI');
 
+mongoose.connect(
+    process.env.mongodburl,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
 const db = mongoose.connection;
 
 db.on('error',console.log.bind(console,'error on connecting to the database'));
